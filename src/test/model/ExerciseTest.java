1b3860c6e2444641d0b3daae59884bdea108d7fa
Kashish Rohira <kashishrohira2004@gmail.com>
2023-10-23 21:29:00 -0700

    jsonToWorkoutLog added

--- src/test/model/ExerciseTest.java
+++ src/test/model/ExerciseTest.java
@@ -5,11 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExerciseTest {
+    public Exercise testExercise;
+
     @Test // testing Exercise constructor
     public void testConstructor() {
-        Exercise testExercise = new Exercise("Squats",10);
+        testExercise = new Exercise("Squats",10);
         assertEquals("Squats", testExercise.getExerciseName());
         assertEquals(10, testExercise.getReps());
+    }
+
+    @Test
+    public void testSetExerciseName() {
+        testExercise = new Exercise("squat",10);
+        testExercise.setExerciseName("lunge");
+        assertEquals("lunge", testExercise.getExerciseName());
+    }
+
+    @Test
+    public void testSetExerciseReps() {
+        testExercise = new Exercise("squat",10);
+        testExercise.setExerciseReps(20);
+        assertEquals(20, testExercise.getReps());
     }
 
 }
